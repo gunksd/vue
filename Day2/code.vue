3c6b0ffde11e013fc6dcdfa431d7d45f76b1ce82
Awan Smith <3056571032@qq.com>
2024-07-28 22:00:43 +0800

    Day2

---
+++ Day2/code.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref,reactive } from "vue";
+const titleclass = ref('title')
+const Count=ref('count')
+const counter = reactive({
+    count:0
+})
+const message = ref ('Make me red!')
+const increment = () =>{
+    counter.count++
+}
+</script>
+
+<template>
+<button @click="increment">
+  <h1 :class="titleclass">
+    {{ message }}
+  </h1>
+</button>
+<p :class="Count">
+    Count is :{{ counter.count }}
+</p>
+</template>
+
+<style>
+.title {
+  color: red;
+}
+.count {
+    color: blue;
+}
+</style>
---
+++ Day2/summary.md
@@ -0,0 +1,5 @@
+1.reactive({count:0})
+  最外面要加圆括号
+2.属性的绑定如果是响应式需要有：（v-bind的缩写）
+3.import { ref,reactive } from "vue";缺少会报错
+4.GPT归档链接：https://chatgpt.com/share/15de522b-c702-4a0f-9751-c4d4ae27fbf3
