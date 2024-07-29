735362c04d0fe556c3486e5847d756752d304570
Awan Smith <3056571032@qq.com>
2024-07-30 00:01:29 +0800

    Day3

---
+++ Day3/code.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+const increment = () => {
+  count.value++
+}
+const count = ref(0)
+</script>
+
+<template>
+    <h1 class="counter">
+  <button class="counter" @click="increment">
+    Count is: {{ count }}
+  </button>
+    </h1>
+</template>
+
+<style>
+.counter{
+   color: rgb(255, 106, 0);
+   background-color: rgb(64, 55, 55); /* 设置按钮背景颜色 */
+   border: none; /*移除默认边框*/
+   padding: 5px 5px;/*增加内边距*/
+   border-radius: 5px;/*圆角边框*/
+   display: flex;/*将按钮变成弹性容器？*/
+   align-items: center; /* 垂直居中 */
+   justify-content: center; /* 水平居中 */
+   width: 150px; /* 根据需要设置宽度 */
+   height: 50px; /* 根据需要设置高度 */
+}
+</style>
---
+++ Day3/summary.md
@@ -0,0 +1,4 @@
+1.弹性布局：display: flex 将按钮变成一个弹性容器。=>垂直和水平居中
+2.<button class="counter" @click="increment">
+  不需要在<script setup>里面定义变量。
+3.GPT link：https://chatgpt.com/share/f2dee54a-4e2f-4fff-84b1-162fbd3844c5
