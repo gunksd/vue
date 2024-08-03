db0284ddef0670b0d3c64f8d362b3d019301a0b9
Awan Smith <3056571032@qq.com>
2024-08-03 18:54:10 +0800

    Day8

---
+++ Day8/code.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref,onMounted } from 'vue'
+onMounted(() => {
+  pElementRef.value.textContent = 'message.value'
+})
+const pElementRef = ref(null)
+</script>
+
+<template>
+  <p ref="pElementRef">Hello</p>
+</template>
