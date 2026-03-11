a0a3e2f28e443cb89dd32e76d5b69162030c4d0c
Awan Smith <114794418+gunksd@users.noreply.github.com>
2026-03-11 22:24:55 +0800

    Update code.vue

--- Day2/code.vue
+++ Day2/code.vue
@@ -1,32 +1,59 @@
 <script setup>
-import { ref,reactive } from "vue";
-const titleclass = ref('title')
-const Count=ref('count')
-const counter = reactive({
-    count:0
-})
-const message = ref ('Make me red!')
-const increment = () =>{
-    counter.count++
+import {ref} from "vue"
+
+const isActive = ref(false)
+const counter = ref(0)
+const Title = ref("title")
+const message = ref('RED')
+const counterClass = ref('counter')
+
+const increment = () => {
+      counter.value++
+}
+
+const toggle = () => {
+  isActive.value = !isActive.value
 }
 </script>
 
 <template>
-<button @click="increment">
-  <h1 :class="titleclass">
-    {{ message }}
-  </h1>
-</button>
-<p :class="Count">
-    Count is :{{ counter.count }}
-</p>
+
+  <button @click="increment">
+    <h1 :class="Title">
+      {{ message }}
+    </h1>
+  </button>
+
+  <button @click="toggle(),increment()"> 
+    <!--工程化做法是封装成一个函数：
+      const handleclick = () =>{
+            toggle()
+            increment()
+      } 
+    然后绑定直接@click="handleclick"
+    -->
+    <h2 :class=" {active: isActive} ">
+      {{ Title }}
+    </h2>
+  </button>
+
+  <p :class="counterClass">
+
+  计数是：{{ counter }}
+
+  </p>
 </template>
 
 <style>
 .title {
   color: red;
 }
-.count {
-    color: blue;
+
+.counter {
+  color: blue;
+}
+
+.active {
+  color: blueviolet;
 }
 </style>
